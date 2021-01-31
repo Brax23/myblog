fe24f1ab04f006916d0f7fc864d44f433612198b
volodyabr <you@example.com>
2021-01-31 17:29:35 +0300

    Add Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=timezone.now())
+    image = models.ImageField(upload_to='blog_images/%Y/%m/%d/')
